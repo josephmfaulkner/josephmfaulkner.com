cdcc4cd35375fd78781476ab9f1f7921c7af509d
josephmfaulkner <josephmfaulkner@gmail.com>
2021-07-26 22:23:29 -0700

    json data

---
+++ gatsby-browser.js
@@ -0,0 +1,2 @@
+import './src/scss/App.scss';
+import './node_modules/@fortawesome/fontawesome-free/css/all.css';
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: "josephmfaulkner.com",
+    title: "Joseph Faulkner",
+    description: "My personal webpage",
+    favicon: "none"
   },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    `gatsby-transformer-json`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: `site-data`,
+        path: `${__dirname}/src/site-data/`,
       },
-      __key: "images",
-    },
+    }
   ],
 };
