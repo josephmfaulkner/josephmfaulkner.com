5d3cc5df524b1bb1001ff5d6dcb27d664fe558d9
josephmfaulkner <josephmfaulkner@gmail.com>
2021-08-02 19:19:45 -0700

    Social Media Links

--- src/components/socialMediaLinks.js
+++ src/components/socialMediaLinks.js
@@ -1,8 +1,5 @@
 import * as React from "react"
-
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 import { useStaticQuery, graphql } from "gatsby";
 
