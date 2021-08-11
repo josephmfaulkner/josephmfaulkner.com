12f4d2d61c3fec56b5a5e58f76ce30db041537e1
josephmfaulkner <josephmfaulkner@gmail.com>
2021-08-10 19:07:25 -0700

    Project and Profile section

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,16 +5,11 @@ const ProjectCard = (props) => {
   const projectData = props.projectData;
   return (
     <div className="col">
-        <div className="card shadow-sm">
-
-              
-
-
-
-            <div className="card-body">
-
-                <h5 className="card-text text-center">{projectData.title}</h5>
-
+        <div className="card shadow-sm" style={{'height' : '100%'}} >
+            <div className="card-header">
+              <h5 className="card-text text-center">{projectData.title}</h5>
+            </div>
+            <div className="card-body" >
               <img
                   className="projectImage"
                   src={projectData.image_url}
@@ -24,16 +19,18 @@ const ProjectCard = (props) => {
                   width="100%"
                 />
                 <p className="card-text">{projectData.description}</p>
+            </div>
+            <div className="card-footer">
                 <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group projectCardButtons">
-                  <Nav.Link title="Demo" className="btn btn-md btn-outline-secondary" href={projectData.demo_link} target="_blank"> 
-                    <i className="fas fa-laptop-code"></i>
-                  </Nav.Link>
-                  <Nav.Link title="Source code" className="btn btn-md btn-outline-secondary" href={projectData.code_link} target="_blank">
-                    <i className="fab fa-git-alt"></i>
-                  </Nav.Link>
-                </div>
-                <small className="text-muted">{projectData.tech_used}</small>
+                  <div className="btn-group projectCardButtons">
+                    <Nav.Link title="Demo" className="btn btn-md btn-outline-secondary" href={projectData.demo_link} target="_blank"> 
+                      <i className="fas fa-laptop-code"></i>
+                    </Nav.Link>
+                    <Nav.Link title="Source code" className="btn btn-md btn-outline-secondary" href={projectData.code_link} target="_blank">
+                      <i className="fab fa-git-alt"></i>
+                    </Nav.Link>
+                  </div>
+                  <small className="text-muted text-right" style={{'textAlign' : 'right'}}>{projectData.tech_used}</small>
                 </div>
             </div>
         </div>
--- src/pages/_aboutSection.js
+++ src/pages/_aboutSection.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // markup
 const AboutSection = () => {
@@ -8,14 +10,21 @@ const AboutSection = () => {
     <div id="about" className="section">
 
       <Container className="home-page-content">
-        <h1 className="display-4 fw-normal">Howdy,</h1>
-        <p>
-        I have over five years of professional experience developing and maintaining software systems that store, retrieve and process business information. 
-        <br></br>
-        As a Fullstack Software Developer, I have been responsible for supporting service-desk which has given me extensive experience interacting with users to diagnose and resolve software errors. I have also coordinated with Business Analysts and software users to develop and implement solutions that meet the needs of the company.
-        I don’t have a particular goal with this website. I just wanted a place to show off my stuff. 
-        Hope you enjoy exploring my work.  
-        </p>
+        <Row className="justify-content-center">
+          <Col xs={{span: 12, order: 0}} md={{span: 6, order: 1}}  lg={{span: 4, order: 1}}  xl={{span: 3, order: 1}} >
+              <img src="/images/profile-2.png" className="img-thumbnail z-depth-2" alt="Joseph Faulkner"/>
+          </Col>  
+          <Col xs={{span: 12, order: 1}} md={{span: 6, order: 0}} lg={{span: 8, order: 0}} xl={{span: 9, order: 0}}>
+              <h1 className="display-4 fw-normal">Howdy,</h1>
+              <p>
+              I have over five years of professional experience developing and maintaining software systems that store, retrieve and process business information. 
+              <br></br>
+              As a Fullstack Software Developer, I have been responsible for supporting service-desk which has given me extensive experience interacting with users to diagnose and resolve software errors. I have also coordinated with Business Analysts and software users to develop and implement solutions that meet the needs of the company.
+              I don’t have a particular goal with this website. I just wanted a place to show off my stuff. 
+              Hope you enjoy exploring my work.  
+              </p>
+          </Col>
+        </Row>  
       </Container>
 
     </div>
