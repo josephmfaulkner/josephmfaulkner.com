0d7c7b0c027e6b6241b774fc985a38b81906bc00
josephmfaulkner <josephmfaulkner@gmail.com>
2021-09-22 14:20:51 -0700

    changed about section

--- src/pages/_aboutSection.js
+++ src/pages/_aboutSection.js
@@ -19,7 +19,6 @@ const AboutSection = () => {
               <p>
               I have over five years of professional experience developing and maintaining software systems that store, retrieve and process business information. 
               <br></br>
-              As a Fullstack Software Developer, I have been responsible for supporting service-desk which has given me extensive experience interacting with users to diagnose and resolve software errors. I have also coordinated with Business Analysts and software users to develop and implement solutions that meet the needs of the company.
               I don’t have a particular goal with this website. I just wanted a place to show off my stuff. 
               Hope you enjoy exploring my work.  
               </p>
